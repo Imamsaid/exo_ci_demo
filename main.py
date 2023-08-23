1d9f1b855dba3670da2b889123e41f49810651e5
=Imam_said <saidouhw02@gmail.com>
2023-08-23 00:46:55 +0000

    ajout des news files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from script import *
+from script import download_data
 
-url = 'https://www.kaggle.com/datasets/joebeachcapital/australian-population-data-by-region/download?datasetVersionNumber=1'
-download_file(url)
+url = 'https://data.humdata.org/dataset/955ab54d-7369-441b-8d39-a2cdb14a14a1/resource/46940d42-d135-4837-823a-50cd3097c8fe/download/millennium_development_goals_mdgs_indicators_sen.csv'
+download_data(url)
