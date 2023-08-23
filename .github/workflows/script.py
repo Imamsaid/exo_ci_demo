fa96fd9cafc8bcc89aebbaf5c8061c4910ac1e3f
=Imam_said <saidouhw02@gmail.com>
2023-08-23 00:31:19 +0000

    ajout file for dowload

--- .github/workflows/script.py
+++
@@ -1,8 +0,0 @@
-from main import *
-import requests
-
-
-def dowload_file(url,dataset= "data_set"):
-    reponse =requests.get(url)
-    with open(dataset, 'wb') as file:
-        file.write(reponse.content)
