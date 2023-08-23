aa7bf8dfb833852a416f7db88b97639583ef0e64
imam-said <saidhw95@gmail.com>
2023-08-23 23:24:18 +0000

    first test

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-
-import requests
-
-# url = 'https://data.humdata.org/dataset/955ab54d-7369-441b-8d39-a2cdb14a14a1/resource/46940d42-d135-4837-823a-50cd3097c8fe/download/millennium_development_goals_mdgs_indicators_sen.csv'
-# download_data(url)
+def salutation():
+    print("Salam tout le monde")
