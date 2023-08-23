489515b01bd5f6e00d0baf3f2937ae660e3790b7
imam-said <saidhw95@gmail.com>
2023-08-23 22:21:51 +0000

    test

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import requests
 
-def download_file(url, dataset="data_set"):
-    response = requests.get(url)
-    with open(dataset, 'wb') as file:
-        file.write(response.content)
+#def download_file(url, dataset="data_set"):
+  #  response = requests.get(url)
+  #  with open(dataset, 'wb') as file:
+  #      file.write(response.content)
