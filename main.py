7a1a07aee9eb4119ee1e77182d0bbab77a645206
imam-said <saidhw95@gmail.com>
2023-08-24 16:47:49 +0000

    dowload file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from script import download_file, clean_data, removeUnusedColumns
 
 #url = 'https://data.humdata.org/dataset/955ab54d-7369-441b-8d39-a2cdb14a14a1/resource/46940d42-d135-4837-823a-50cd3097c8fe/download/millennium_development_goals_mdgs_indicators_sen.csv'
 
-url = 'https://docs.google.com/spreadsheets/d/1hNJGk3T9lp1e4jQsczf3T_IE9rDVvzvqI3HVz_9GWMA/edit#gid=0'
+url = 'https://drive.google.com/file/d/1Gqi0--vtp3vHR8CyhCH3LVTvdzj9fZIk/view?usp=drive_link'
 file_path = 'data.csv'
 df=download_file(url, file_path)
 
